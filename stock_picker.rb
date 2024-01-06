801060545a5c520a94f594d4a3a06e6b44eb5d1b
Jonatan Cid <86532479+jcidp@users.noreply.github.com>
2024-01-05 19:01:52 -0600

    Solve the Stock Picker project

---
+++ stock_picker.rb
@@ -0,0 +1,18 @@
+def stock_picker(prices)
+  max_profit = 0
+  buy_index = 0
+  sell_index = 1
+  prices.each_index do |i|
+    prices[i+1..-1].each_index do |j|
+      profit = prices[prices.length - j - 1] - prices[i]
+      if profit > max_profit
+        max_profit = profit 
+        buy_index = i
+        sell_index = j
+      end
+    end
+  end
+  [buy_index, sell_index]
+end
+
+p stock_picker([17,3,6,9,15,8,6,1,10])
