90386162aca61615ec2fe10ea5202bc02af993ef
Jonatan Cid <86532479+jcidp@users.noreply.github.com>
2024-02-16 21:17:29 -0600

    Refactor Caesar Cipher

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,15 +1,18 @@
+# frozen_string_literal: true
+
 def caesar_cipher(string, shift_factor)
-  cipher = ""
-  string.each_char do |char| 
-    if !char.ord.between?(65,90) & !char.ord.between?(97,122)
-      cipher += char
-    elsif char.ord.between?(65,90) && !(char.ord + shift_factor).between?(65,90) || char.ord.between?(97,122) && !(char.ord + shift_factor).between?(97,122)
-      cipher += (char.ord + shift_factor - 26).chr
-    else
-      cipher += (char.ord + shift_factor).chr
-    end
+  string.each_char.reduce("") do |cipher, char|
+    shifted = char.ord + shift_factor
+    cipher +
+      case char.ord
+      when 65..90
+        (shifted.between?(65, 90) ? shifted : shifted - 26).chr
+      when 97..122
+        (shifted.between?(97, 122) ? shifted : shifted - 26).chr
+      else
+        char
+      end
   end
-  cipher
 end
 
 p caesar_cipher("What a string!", 5)
